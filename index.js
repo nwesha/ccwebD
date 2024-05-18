ccecbf4d5c70ec3b33fea2110064318b67a36bb0
nwesha <167685600+nwesha@users.noreply.github.com>
2024-05-18 20:30:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+fetch('https://coding-week-2024-api.onrender.com/api/data')
+  .then(response => {
+    // Handle the response data here
+    // document.write(JSON.stringify(response.text()))
+  })
+  .catch(error => {
+    // Handle errors
+  });
